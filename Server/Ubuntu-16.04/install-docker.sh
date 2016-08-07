e5d2c8c5eea3b9b0982129d17ed2bc28a1fa8a4b
Erni Durdevic <erni.durdevic@gmail.com>
2016-08-07 23:12:00 +0200

    Updated scripts for Ubuntu 16.04

--- Server/Ubuntu-16.04/install-docker.sh
+++ Server/Ubuntu-16.04/install-docker.sh
@@ -5,7 +5,7 @@ sudo apt-get install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >> /etc/apt/sources.list.d/docker.list
 
 sudo apt-get update
 
@@ -20,7 +20,7 @@ sudo apt-get install docker-engine
 
 sudo groupadd docker
 
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker root
 
 echo "Now logout and login again... Docker installation complete."
 
